4460fe674e4801c285728f17dacc7733e05fd1c3
Anton Bahdanovich <bumbarashka@users.noreply.github.com>
2017-03-10 19:16:22 -0500

    Solving 514 problem via dynamic programming approach.

--- src/by/bogdan/leetcode/Solution514.java
+++ src/by/bogdan/leetcode/Solution514.java
@@ -3,18 +3,56 @@ package by.bogdan.leetcode;
 import java.util.*;
 
 /**
- * Solution for https://leetcode.com/problems/freedom-trail
+ * Solution for 'Freedom Trail' algorithmic problem.
+ * @see <a href="https://leetcode.com/problems/freedom-trail">514. Freedom Trail (on leetcode.com)</a>
  */
 public class Solution514 {
-    // DP solution
+    /**
+     * Solution using dynamic programming. O(n * m) efficiency and O(n * m) memory usage where <code>n = ring.length()</code>
+     * and <code>m = key.length()</code>
+     * @param ring
+     *          a string, which represents the code engraved on the outer ring
+     * @param key
+     *          string key, which represents the keyword needs to be spelled
+     * @return the minimum number of steps in order to spell all the characters in the keyword
+     */
     public int findRotateSteps(String ring, String key) {
-        // TODO: implement me
-        assert false;
-        return 0;
+        int ringLength = ring.length();
+        int keyLength = key.length();
+        int[][] dp = new int[keyLength + 1][ringLength];
+        Map<Character, List<Integer>> characterPositions = getCharacterPositions(ring);
+        for (int i = keyLength - 1; i >= 0; i--) {
+            char keyChar = key.charAt(i);
+            for (int j = 0; j < ring.length(); j++) {
+                int minSteps = Integer.MAX_VALUE;
+                for(Integer pos: characterPositions.get(keyChar)) {
+                    int steps = Math.abs(pos - j);
+                    steps = Math.min(steps, ringLength - steps) + dp[i + 1][pos];
+                    if (steps < minSteps) {
+                        minSteps = steps;
+                    }
+                }
+                dp[i][j] = minSteps;
+            }
+        }
+        return dp[0][0] + keyLength;
     }
 
-    // Recursive with memoization solution;
+    /**
+     * Recursive solution with memoization. O(n * m) efficiency and O(n * m) memory usage where <code>n = ring.length()</code>
+     * and <code>m = key.length()</code>
+     * @param ring
+     *          a string, which represents the code engraved on the outer ring
+     * @param key
+     *          string key, which represents the keyword needs to be spelled
+     * @return the minimum number of steps in order to spell all the characters in the keyword
+     */
     public int findRotateSteps2(String ring, String key) {
+        Map<Character, List<Integer>> characterPositions = getCharacterPositions(ring);
+        return getMinSteps(ring.length(), characterPositions, 0, key, 0, new HashMap<>());
+    }
+
+    private Map<Character, List<Integer>> getCharacterPositions(String ring) {
         Map<Character, List<Integer>> characterPositions = new HashMap<>();
         for (int i = 0; i < ring.length(); i++) {
             Character c = ring.charAt(i);
@@ -25,10 +63,11 @@ public class Solution514 {
             }
             positions.add(i);
         }
-        return getMinSteps(ring.length(), characterPositions, 0, key, 0, new HashMap<>());
+        return characterPositions;
     }
 
-    private int getMinSteps(Integer length, Map<Character, List<Integer>> characterPositions, Integer position, String key, int keyPosition, Map<Integer, Integer> results) {
+    private int getMinSteps(Integer length, Map<Character, List<Integer>> characterPositions, Integer position,
+                            String key, int keyPosition, Map<Integer, Integer> results) {
         Integer resultKey = position * 100 + keyPosition;
         if (results.containsKey(resultKey)) {
             return results.get(resultKey);
@@ -40,7 +79,8 @@ public class Solution514 {
         if (positions.size() == 1) {
             Integer newPosition = positions.get(0);
             Integer distance = Math.abs(position - newPosition);
-            Integer result = 1 + Math.min(distance, length - distance) + getMinSteps(length, characterPositions, newPosition, key, keyPosition + 1, results);
+            Integer result = 1 + Math.min(distance, length - distance)
+                    + getMinSteps(length, characterPositions, newPosition, key, keyPosition + 1, results);
             results.put(resultKey, result);
             return result;
         } else {
--- test/by/bogdan/leetcode/Solution514Test.java
+++ test/by/bogdan/leetcode/Solution514Test.java
@@ -6,6 +6,14 @@ import static org.junit.Assert.*;
 
 public class Solution514Test {
     @Test
+    public void findRotateSteps() throws Exception {
+        assertEquals(4, new Solution514().findRotateSteps("godding", "gd"));
+        assertEquals(204, new Solution514().findRotateSteps("xrrakuulnczywjs", "jrlucwzakzussrlckyjjsuwkuarnaluxnyzcnrxxwruyr"));
+        assertEquals(6, new Solution514().findRotateSteps("abcde", "ade"));
+        assertEquals(137, new Solution514().findRotateSteps("caotmcaataijjxi", "oatjiioicitatajtijciocjcaaxaaatmctxamacaamjjx"));
+    }
+
+    @Test
     public void findRotateSteps2() throws Exception {
         assertEquals(4, new Solution514().findRotateSteps2("godding", "gd"));
         assertEquals(204, new Solution514().findRotateSteps2("xrrakuulnczywjs", "jrlucwzakzussrlckyjjsuwkuarnaluxnyzcnrxxwruyr"));
