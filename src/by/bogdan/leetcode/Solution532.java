7fd409cbad70db9b08041a9953af70ddd13c1964
Anton Bahdanovich <bumbarashka@users.noreply.github.com>
2017-03-11 11:41:08 -0500

    Implementing new approach for Solution 532 (K-diff Pairs in an Array).Adjusting .gitignore for hidden solution.

--- src/by/bogdan/leetcode/Solution532.java
+++ src/by/bogdan/leetcode/Solution532.java
@@ -50,4 +50,37 @@ public class Solution532 {
         }
         return count;
     }
+
+    /**
+     * Efficiency is O(n). Memory usage is O(n).
+     *
+     * @param nums array of integers
+     * @param k    an integer k
+     * @return the number of unique k-diff pairs in the array
+     */
+    public int findPairs2(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 0) {
+            return 0;
+        }
+        int count = 0;
+        Map<Integer, Integer> numsToCount = new HashMap<>();
+        for(int i: nums) {
+            numsToCount.put(i, numsToCount.getOrDefault(i, 0) + 1);
+        }
+        for (Map.Entry<Integer,Integer> entry: numsToCount.entrySet()) {
+            if (k == 0) {
+                // zero target can only be if some value is repeated at least once
+                if(entry.getValue() > 1) {
+                    count++;
+                }
+            } else {
+                // It checks only <code>entry.getKey() + k</code> because <code>entry.getKey() - k</code> map's key will
+                // be checked within another iteration if it exists
+                if(numsToCount.containsKey(entry.getKey() + k)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
--- test/by/bogdan/leetcode/Solution532Test.java
+++ test/by/bogdan/leetcode/Solution532Test.java
@@ -6,11 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 public class Solution532Test {
     @Test
-    public void testSomething() throws Exception {
+    public void findPairs() throws Exception {
         assertEquals(2, new Solution532().findPairs(new int[]{3, 1, 4, 1, 5}, 2));
         assertEquals(4, new Solution532().findPairs(new int[]{1, 2, 3, 4, 5}, 1));
         assertEquals(1, new Solution532().findPairs(new int[]{1, 3, 1, 5, 4}, 0));
         assertEquals(0, new Solution532().findPairs(new int[]{1, 3, 1, 5, 4}, -1));
         assertEquals(1, new Solution532().findPairs(new int[]{1, 1, 1, 2, 1}, 1));
+    }
+
+    @Test
+    public void findPairs2() throws Exception {
+        assertEquals(2, new Solution532().findPairs2(new int[]{3, 1, 4, 1, 5}, 2));
+        assertEquals(4, new Solution532().findPairs2(new int[]{1, 2, 3, 4, 5}, 1));
+        assertEquals(1, new Solution532().findPairs2(new int[]{1, 3, 1, 5, 4}, 0));
+        assertEquals(0, new Solution532().findPairs2(new int[]{1, 3, 1, 5, 4}, -1));
+        assertEquals(1, new Solution532().findPairs2(new int[]{1, 1, 1, 2, 1}, 1));
     }
 }
