f32b417419e6318865d791c5e781e1ebf43ededd
Anton Bahdanovich <bumbarashka@users.noreply.github.com>
2017-11-19 21:14:18 -0500

    Adjusting comment

--- src/by/bogdan/leetcode/Solution726.java
+++ src/by/bogdan/leetcode/Solution726.java
@@ -57,8 +57,10 @@ public class Solution726 {
      * <p>
      * Space Complexity: O(N)
      *
-     * @param formula
-     * @return
+     * @param formula a formula
+     * @return the count of all elements as a string in the following form: the first name (in sorted order),
+     * followed by its count (if that count is more than 1), followed by the second name (in sorted order),
+     * followed by its count (if that count is more than 1), and so on.
      */
     public String countOfAtoms(String formula) {
         char[] characters = formula.toCharArray();
